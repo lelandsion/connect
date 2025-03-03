f2d98b446e188e07afb7ed01aa68c9e8253b84f8
Leland Sion <lelandsion@gmail.com>
2025-03-02 17:19:52 -0800

    feat: Created api routes and for sensors, and implemented server.js.

--- backend/Server.js
+++ backend/Server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 const uri = process.env.MONGODB_URI;
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5003;
 const clientOptions = { };
 
 // Middleware
@@ -23,16 +23,16 @@ mongoose.connect(uri, clientOptions)
     })
     .catch(err => console.error('MongoDB connection error:', err));
 
-const authRoutes = require('./routes/auth');
+// const authRoutes = require('./routes/auth'); // Add this later
 const sensorRoutes = require('./routes/sensors');  // New route for sensors
-const sensorDataRoutes = require('./routes/sensorData');  // New route for sensor readings
-const userRoutes = require('./routes/user');
+// const sensorDataRoutes = require('./routes/sensorData');  // New route for sensor readings
+// const userRoutes = require('./routes/user');
 
 // Route handlers
-app.use('/api/auth', authRoutes);
+// app.use('/api/auth', authRoutes); // Add this later
 app.use('/api/sensors', sensorRoutes);  // Manage IoT sensors
-app.use('/api/sensor-data', sensorDataRoutes);  // Store & retrieve sensor readings
-app.use('/api/user', userRoutes);
+//app.use('/api/sensor-data', sensorDataRoutes);  // Store & retrieve sensor readings
+//app.use('/api/user', userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
